19b4b5bdcf04a51cc8ea3fec60b45ab954098b82
carcasonne <villumsonne@tutanota.com>
2023-03-03 12:51:35 +0100

    Made Program.py

---
+++ Program/Program.py
@@ -0,0 +1,14 @@
+print("Beginning")
+
+print("Parsing")
+
+print("Parsed nurses:")
+
+print("Running through knapsack...")
+
+
+print("Tabu searching...")
+
+print("Network flowing...")
+
+print("Done")
