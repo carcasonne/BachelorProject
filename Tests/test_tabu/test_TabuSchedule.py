245d85e75accc26140d432831c30c845be12c191
SadlifeCaw <caws@itu.dk>
2023-03-14 16:13:39 +0100

    yes

--- Tests/test_tabu/test_TabuSchedule.py
+++ Tests/test_tabu/test_TabuSchedule.py
@@ -2,8 +2,13 @@ import unittest
 
 from Domain.Models.Enums.Contract import Contract
 from Domain.Models.Enums.Grade import Grade
+from Domain.Models.Enums.ShiftType import ShiftType
 from Domain.Models.Nurse import Nurse
+from Domain.Models.Schedule import Schedule
+from Domain.Models.Shift import Shift
 from Domain.Models.Tabu.TabuNurse import TabuNurse
+from Domain.Models.Tabu.TabuSchedule import TabuSchedule
+from TestData import TabuSetup
 
 
 class TabuScheduleTests(unittest.TestCase):
