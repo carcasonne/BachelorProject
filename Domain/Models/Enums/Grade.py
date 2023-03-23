e3c5599d7bf19845cd6ea2c39a67b0c813a7858f
carcasonne <villumsonne@tutanota.com>
2023-03-23 15:16:00 +0100

    Add domain tests

--- Domain/Models/Enums/Grade.py
+++ Domain/Models/Enums/Grade.py
@@ -1,7 +1,14 @@
 from enum import Enum
+from functools import total_ordering
 
-
+@total_ordering
 class Grade(Enum):
     ONE = 1
     TWO = 2
     THREE = 3
+
+    def __lt__(self, other):
+        if self.__class__ is other.__class__:
+            return self.value < other.value
+        else:
+            return False
