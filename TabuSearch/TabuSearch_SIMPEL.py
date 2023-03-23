be23f68c6b9d602dbeb25ec4e1492602f8784bd2
Mads <mpia@itu.dk>
2023-03-23 12:18:15 +0100

    Stopping random descent from moving day to night and vice vbersa

--- TabuSearch/TabuSearch_SIMPEL.py
+++ TabuSearch/TabuSearch_SIMPEL.py
@@ -94,13 +94,14 @@ class TabuSearch_SIMPLE:
         for nurse in schedule.nurses:
             if nurse not in self.tabuList:
                 for pattern in self.feasiblePatterns[nurse.id]:
-                    neighbour = copy.deepcopy(schedule)
-                    n_nurse = neighbour.nurses[nurse.id]
-                    n_nurse.assignShiftPattern(pattern)
-                    evaluateCC(neighbour)
-                    evaluatePC(neighbour)
-                    if neighbour.CC < schedule.CC and neighbour.PC <= schedule.PC:
-                        return neighbour
+                    if (nurse.worksNight and pattern(0) == [0] * 7) or (not nurse.worksNight and pattern(0) != [0] * 7):
+                        neighbour = copy.deepcopy(schedule)
+                        n_nurse = neighbour.nurses[nurse.id]
+                        n_nurse.assignShiftPattern(pattern)
+                        evaluateCC(neighbour)
+                        evaluatePC(neighbour)
+                        if neighbour.CC < schedule.CC and neighbour.PC <= schedule.PC:
+                            return neighbour
         return None
 
     def balanceRestoring(self, schedule):
