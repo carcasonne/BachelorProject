21310f05574147cd55c0a0592e9bc6ee1b544a34
Casper Skourup <casper.skourup@gmail.com>
2023-03-25 22:38:05 +0100

    Balance Restoring Template Started

--- README.md
+++ README.md
@@ -8,4 +8,5 @@ This the the git repository containing the code for the Bachelor project by...
 
 Datsets can be found in /Data
 
-Example .json files can be found in /Data/Example, while datsets taken from the Nurse Rostering Competition can be found in /Data/NurseRosteringCompetition
+Example .json files can be found in /Data/Example, while datsets taken from the Nurse Rostering Competition can be found
+in /Data/NurseRosteringCompetition
--- TabuSearch/TabuSearch_SIMPEL.py
+++ TabuSearch/TabuSearch_SIMPEL.py
@@ -8,10 +8,12 @@ from Domain.Models.Tabu.TabuSchedule import TabuSchedule
 from Domain.Models.Enums.Grade import Grade
 from TabuSearch.StaticMethods import *
 
+
 # TODO: THIS SOLUTION IS ONLY BASED ON GRADE THREE
 
 class TabuSearch_SIMPLE:
-    def __init__(self, initialSolution): # (initialSolution, solutionEvaluator, neighborOperator, aspirationCriteria, acceptableScoreThreshold, tabuTenure)
+    def __init__(self,
+                 initialSolution):  # (initialSolution, solutionEvaluator, neighborOperator, aspirationCriteria, acceptableScoreThreshold, tabuTenure)
         """
         The next three variables is there to make sure that we hold the 3 tabu criteria:
             1) A move involves the tabu nurse (i.e. the nurse moved last time) from tabuList.
@@ -21,7 +23,7 @@ class TabuSearch_SIMPLE:
         # Tabu criteria 1: tabulist - Nurses involved in earlier moves - This is a fixed length of 1
         self.tabuList = []
         # Tabu criteria 2:dayNightTabuList - Nurses working days - list[set(nurse working days), ...] - Length 6 (binary representation)
-        self.dayNightTabuList = []
+        self.dayNightTabuList = []  # TODO: Maybe this should be an array of sets for makeing easy and quick comparisons
         # Tabu criteria 3:
         self.dayNightCounter = 0
         self.maxits = 50
@@ -41,19 +43,21 @@ class TabuSearch_SIMPLE:
 
         self.shiftRequirements = findShiftTypeRequirements(initialSolution)
 
-    def initSchedule(self): # Just assigning the first pattern to every nurse for testing purposes, instead of randomized, since it solves it too fast with a randomized initial solution.
+    def initSchedule(
+            self):  # Just assigning the first pattern to every nurse for testing purposes, instead of randomized, since it solves it too fast with a randomized initial solution.
         for nurse in self.bestSolution.nurses:
             randomizeConstraints(nurse)
             # self.bestSolution.assignPatternToNurse(nurse, self.feasiblePatterns[nurse.id][0])
-            self.bestSolution.assignPatternToNurse(nurse, self.feasiblePatterns[nurse.id][random.randint(0, len(self.feasiblePatterns[nurse.id]) - 1)])
+            self.bestSolution.assignPatternToNurse(nurse, self.feasiblePatterns[nurse.id][
+                random.randint(0, len(self.feasiblePatterns[nurse.id]) - 1)])
 
     def makeMove(self, move):
         if move is None:
             return None
         else:
-            if move[1]: # If move changes the day night split
+            if move[1]:  # If move changes the day night split
                 dayNurses = []
-                for nurse in self.bestSolution.nurses: # Find all nurses that works during the day
+                for nurse in self.bestSolution.nurses:  # Find all nurses that works during the day
                     if not nurse.worksNight:
                         dayNurses.append(nurse.id)
                 self.dayNightTabuList.append(dayNurses)
@@ -78,10 +82,9 @@ class TabuSearch_SIMPLE:
                             if self.makeMove(self.underCovering(self.bestSolution)) is None:
                                 self.makeMove(self.randomKick(self.bestSolution))
 
-
     # Phase 1 Moves:
-
     # TODO: Random decent after PC and LB
+    # TODO: Is Tabu criteria 2 taken in to count here?
     def randomDecent(self, schedule):
         """
         Step 1.1 (Random decent). Carry out random decent by accepting the first neighbourhood move that satisfies
@@ -93,8 +96,10 @@ class TabuSearch_SIMPLE:
         for nurse in schedule.nurses:
             if nurse.id not in self.tabuList:
                 for pattern in self.feasiblePatterns[nurse.id]:
-                    if (nurse.worksNight and pattern.day == [0] * 7) or (not nurse.worksNight and pattern.day != [0] * 7):
-                        if calculateDifferenceCC(schedule, nurse, pattern) < 0 and calculateDifferencePC(nurse, pattern) <= 0:
+                    if (nurse.worksNight and pattern.day == [0] * 7) or (
+                            not nurse.worksNight and pattern.day != [0] * 7):
+                        if calculateDifferenceCC(schedule, nurse, pattern) < 0 and calculateDifferencePC(nurse,
+                                                                                                         pattern) <= 0:
                             neighbour = copy.deepcopy(schedule)
                             n_nurse = neighbour.nurses[nurse.id]
                             neighbour.assignPatternToNurse(n_nurse, pattern)
@@ -106,15 +111,58 @@ class TabuSearch_SIMPLE:
 
     def balanceRestoring(self, schedule):
         """
-        Step 1.2 (Balance days and nights). Check for balance by using checkBalance (Eq(5)) and return None if nether days and nights is satisfied. if
+        Step 1.2 (Balance days and nights). Check for balance by using checkBalance (Eq(5)) and return None if nether
+        days and nights is satisfied. Attempt to correct the balance by selection the best move satisfying non-tabu
+        conditions 1 and 2 using a candidate list given by:
+            1. moves that reduce the shortfall on nights without increasing that on days if night are short.
+            2. moves that reduce the shortfall on days without increasing that on nights if days are short.
+        If no such move exists extend neighbourhood to allow a night and day nurse to swap types subject to improving
+        the shortfall of one type without increasing the other. If still not successful relax tabu status and repeat.
         :param schedule:
         :return: move, changed day/night:
         """
         print("Running Balance Restoration...")
-        if checkBalance(self.bestSolution) == (False, False):
+        balance = checkBalance(schedule)
+        moveList = []
+        match balance:
+            case (False, False):  # There are not enough nurses on days or nights
+                return None
+            case (False, True):  # There are not enough nurses on days
+                for nurse in schedule.nurses:
+                    if nurse.id not in self.tabuList and nurse.worksNight == False:
+                        for pattern in self.feasiblePatterns[nurse.id]:
+                            diffCC = calculateDifferenceCC(schedule, nurse, pattern)
+                            if diffCC < 0 and pattern.night != [0] * 7:
+                                # TODO: We need to take tabu criteria 2 into count here
+                                moveList.append((diffCC, (nurse, pattern)))
+            case (True, False):  # There are not enough nurses on nights
+                for nurse in schedule.nurses:
+                    if nurse.id not in self.tabuList and nurse.worksNight == True:
+                        for pattern in self.feasiblePatterns[nurse.id]:
+                            diffCC = calculateDifferenceCC(schedule, nurse, pattern)
+                            if diffCC < 0 and pattern.day != [0] * 7:
+                                # TODO: We need to take tabu criteria 2 into count here
+                                moveList.append((diffCC, (nurse, pattern)))
+            case (True, True):  # There are enough nurses on days and nights
+                pass
+
+        if len(moveList) != 0:
+            move = (None, None)
+            bestCC = 0
+            for m in moveList:
+                if bestCC > m[0]:
+                    move = m
+
+            neighbour = copy.deepcopy(schedule)
+            n_nurse = neighbour.nurses[move[0].id]
+            neighbour.assignPatternToNurse(n_nurse, move[1])
+            self.tabuList = []
+            self.tabuList.append(n_nurse.id)
+            print(neighbour.scores())
+            return neighbour, True
+        else:
+            # TODO: Make method balanceSwap(): swap nurses to allow for balance restoring.
             return None
-
-
 
     def shiftChain(self, schedule):
         """
@@ -163,25 +211,23 @@ class TabuSearch_SIMPLE:
                 print(neighbour.scores())
                 return neighbour, nurseWorkedNight != n_nurse.worksNight
         pass
-        
-    
-    def findBalanceRestoringCandidateList(self, schedule:TabuSchedule):
+
+    def findBalanceRestoringCandidateList(self, schedule: TabuSchedule):
         types = [TabuShiftType.DAY, TabuShiftType.NIGHT]
         # Find out if solution is unbalanced for any grade for any type
 
         for type in types:
-                # Get all nurses working this type of pattern
-                typeNurses = [nurse for nurse in schedule.nurses if nurse.worksNights]
-                # Find number of shifts worked by all nurses of this grade and this type
-                shifts = 0
-                for nurse in typeNurses:
-                    pattern = nurse.shiftPattern.day if type == TabuShiftType.DAY else nurse.shiftPattern.night
-                    shifts += sum(pattern)
+            # Get all nurses working this type of pattern
+            typeNurses = [nurse for nurse in schedule.nurses if nurse.worksNights]
+            # Find number of shifts worked by all nurses of this grade and this type
+            shifts = 0
+            for nurse in typeNurses:
+                pattern = nurse.shiftPattern.day if type == TabuShiftType.DAY else nurse.shiftPattern.night
+                shifts += sum(pattern)
 
-                # Fewer shifts worked than is required?
-                if shifts < self.shiftRequirements[type][Grade.THREE]:
-                    
-                    pass
+            # Fewer shifts worked than is required?
+            if shifts < self.shiftRequirements[type][Grade.THREE]:
+                pass
 
         # This is for considering all grades
         # for grade in Grade:
@@ -198,6 +244,5 @@ class TabuSearch_SIMPLE:
 
         #         # Fewer shifts worked than is required?
         #         if shifts < self.shiftRequirements[type][grade]:
-                    
-        #             pass
 
+        #             pass
